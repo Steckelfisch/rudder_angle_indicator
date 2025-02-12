5c7816b7c265d53efbe88a7218457317bd127ac9
Gert-Jan Braas <braas@steckelfisch.nl>
2025-02-12 16:43:04 +0100

    EningeMonitor kan op meerdere manieren aangemaakt en geinitialiseerd worden

--- EngineMonitor.cpp
+++ EngineMonitor.cpp
@@ -4,25 +4,58 @@
 
 
 // Create a new instance of the oneWire class to communicate with any OneWire device:
-OneWire oneWire(ONE_WIRE_BUS);
-
+// OneWire oneWire(ONE_WIRE_BUS);
 // Pass the oneWire reference to DallasTemperature library:
-DallasTemperature sensors(&oneWire);
+// DallasTemperature sensors(&oneWire);
+// variable to hold device addresses
+DeviceAddress thermometer;
 
-int _flowCount = 0;
-void handleFlowIntHandle()
+WaterFlow coolantWaterFlow; // WATER_FLOW_PIN, 65);
+
+void count()
 {
-  _flowCount++;
+  coolantWaterFlow.pulseCount();
 }
 
-EngineMonitor::EngineMonitor(int pinCoolantWaterFlow, int addrCoolantWaterTemp, int pinOilPress, int addrOilTemp)
+void printAddress(DeviceAddress deviceAddress)
+{ 
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    Serial.print("0x");
+    if (deviceAddress[i] < 0x10) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+    if (i < 7) Serial.print(", ");
+  }
+  Serial.println("");
+}
+
+EngineMonitor::EngineMonitor()
 {
+}
+
+//void EngineMonitor::init(int pinCoolantWaterFlow, int addrCoolantWaterTemp, int pinOilPress, int addrOilTemp)
+void EngineMonitor::init(DallasTemperature _sensors, int pinCoolantWaterFlow, int addrCoolantWaterTemp, int pinOilPress, int addrOilTemp)
+{
+  sensors = _sensors;
   _pinCoolantWaterFlow = pinCoolantWaterFlow;
   _addrCoolantWaterTemp = addrCoolantWaterTemp;
   _pinOilPress = pinOilPress; 
   _addrOilTemp = addrOilTemp;
   _deviceCount = 0;
+}
+
+EngineMonitor::EngineMonitor(DallasTemperature _sensors, int pinCoolantWaterFlow, int addrCoolantWaterTemp, int pinOilPress, int addrOilTemp)
+{
+  sensors = _sensors;
+  _pinCoolantWaterFlow = pinCoolantWaterFlow;
+  _addrCoolantWaterTemp = addrCoolantWaterTemp;
+  _pinOilPress = pinOilPress; 
+  _addrOilTemp = addrOilTemp;
+  _deviceCount = 0;
+  
+
   //_flowCount = 0;
+  // coolantWaterFlow
 }
 
 
@@ -30,12 +63,17 @@ void EngineMonitor::begin()
 {
   if (!_pinCoolantWaterFlow)
   {
-    attachInterrupt(digitalPinToInterrupt(_pinCoolantWaterFlow), handleFlowIntHandle, CHANGE);
+    coolantWaterFlow.init(_pinCoolantWaterFlow, 45);
+    coolantWaterFlow.begin(count);
+    
+    // pinMode(_pinCoolantWaterFlow), INPUT);
+    // digitalWrite(_pinCoolantWaterFlow), INPUT_PULLUP);  
+    // attachInterrupt(digitalPinToInterrupt(_pinCoolantWaterFlow), handleFlowIntHandle, FALLING);
   }
 
   // if (_addrCoolantWaterTemp != 0) || (_addrOilTemp != 0)
-  if (!_addrCoolantWaterTemp || !_addrOilTemp)
-  {
+  //if (_addrCoolantWaterTemp==0 && _addrOilTemp==0)
+  //{
     sensors.begin();
     // Locate the devices on the bus:
     Serial.println("Locating devices...");
@@ -43,11 +81,70 @@ void EngineMonitor::begin()
     _deviceCount = sensors.getDeviceCount();
     Serial.print(_deviceCount);
     Serial.println(" devices");
-  }
+
+    Serial.println("Printing addresses...");
+    for (int i = 0;  i < _deviceCount;  i++)
+    {
+      Serial.print("Sensor ");
+      Serial.print(i+1);
+      Serial.print(" : ");
+      sensors.getAddress(thermometer, i);
+      printAddress(thermometer);
+      sensors.setResolution(i, 9);
+
+    }
+  //}
+  // report parasite power requirements
+	Serial.print("Parasite power is: "); 
+	if (sensors.isParasitePowerMode()) Serial.println("ON");
+	else Serial.println("OFF");
+
 }
 
 bool EngineMonitor::update_state()
 {
+  if (!_pinCoolantWaterFlow)
+  {
+    // coolantWaterFlow.pulseCount();
+  }
+
+  //if (_addrCoolantWaterTemp||_addrOilTemp)
+  //{
+  sensors.requestTemperatures();
+  // delay(1000);
+  //}
+
+  
+  float tempC = sensors.getTempCByIndex(0);
+  if(tempC == DEVICE_DISCONNECTED_C) 
+  {
+		Serial.println("Error: Could not read temperature data");
+		return;
+	} 
+  else 
+  {
+  Serial.print("Temperature: ");
+  Serial.print(tempC);
+  Serial.println("°C");
+  }
+  
+  //if (!_addrCoolantWaterTemp) 
+  //{
+    Serial.print(sensors.getResolution(1));
+    Serial.print(" Koelwater Temp: ");
+    // Serial.print(sensors.getTempC(_addrCoolantWaterTemp));
+    float tmp1 = sensors.getTempCByIndex(1);
+    Serial.print(tmp1);
+    Serial.println("°C ");
+  //}
+
+  //if (!_addrOilTemp)
+  //{
+    Serial.print("Olie Temp: ");
+    // Serial.print(sensors.getTempC(_addrCoolantWaterTemp));
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.println("°C ");
+  //}
   return false;
 }
 
--- rudder_angle_indicator.ino
+++ rudder_angle_indicator.ino
@@ -2,28 +2,38 @@
 #include "RudderController.h"
 #include "EngineMonitor.h"
 
+#define ONE_WIRE_BUS 2
+
 int ledPin = 13;
 bool ledPin13State = HIGH;
 int ledPin13StateTime = 0;
 unsigned long oldTime;
 
+
+// Create a new instance of the oneWire class to communicate with any OneWire device:
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass the oneWire reference to DallasTemperature library:
+DallasTemperature sensors(&oneWire);
+
+
 int pinCoolantWaterFlow = 0;
-int addrCoolantWaterTemp = 0;
+DeviceAddress addrCoolantWaterTemp; // = { 0x28, 0x35, 0xD9, 0x06, 0x0C, 0x00, 0x00, 0x07 };
+                              
 int pinOilPress = 0;
-int addrOilTemp = 0;
+DeviceAddress addrOilTemp; // = { 0x28, 0xFF, 0x7F, 0x72, 0x50, 0x17, 0x04, 0x7D };
 
 int ledSBPin = 7;
 int ledCenterPin = 8;
 int ledBBPin = 9;
 
-int analogPin = 0;
+int rudderanglePin = A0;
 
 
 bool rudderUpdated = false;
 
 
-RudderController rudderController(analogPin);
-EngineMonitor EngineMonitor(pinCoolantWaterFlow, addrCoolantWaterTemp, pinOilPress, addrOilTemp);
+RudderController rudderController(rudderanglePin);
+EngineMonitor engineMonitor; //pinCoolantWaterFlow, addrCoolantWaterTemp, pinOilPress, addrOilTemp);
 // LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3); //, POSITIVE); 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // addres, cols, rows
 
@@ -31,7 +41,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); // addres, cols, rows
 void(* resetFunc) (void) = 0; //declare reset function @ address 0
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
 /*
   lcd.begin(20, 4);// Configuramos el numero de columnas y filas del LCD.
@@ -42,8 +52,11 @@ void setup() {
   lcd.setCursor(6,1);
   lcd.print("Stcklfsch/Wndrlb");
 */
+  //rudderController.begin(); // todo: pass lcd?
+  engineMonitor.init(sensors, pinCoolantWaterFlow, addrCoolantWaterTemp, pinOilPress, addrOilTemp);
+  //engineMonitor.init(pinCoolantWaterFlow, addrCoolantWaterTemp, pinOilPress, addrOilTemp);
 
-  rudderController.begin(); // todo: pass lcd?
+  engineMonitor.begin();
 
   pinMode(ledPin, OUTPUT);
 
@@ -53,16 +66,23 @@ void setup() {
 
 
 void loop() {
-  rudderUpdated = rudderController.update_state();
-  if( rudderUpdated ) 
-  { 
-    rudderController.print_state();
-    delay(50);
+  if(rudderanglePin) 
+  {
+    //rudderUpdated = rudderController.update_state();
+    if( rudderUpdated ) 
+    { 
+      // rudderController.print_state();
+      delay(50);
+    }
   }
   else
   {
     delay(500);
   }
+
+  engineMonitor.update_state();
+  engineMonitor.print_state();
+
 
   ledPin13State = !ledPin13State;
   digitalWrite(ledPin, ledPin13State);
